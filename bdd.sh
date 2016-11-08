17ffbde3c0ca6d38d8e208dbf599382350d5da65
AneoPsy <paultevatheis@gmail.com>
2016-11-07 16:14:43 -1000

    nouveau fichier : .travis.yml  	nouveau fichier : bdd.sh  	nouveau fichier : test_bdd.py

---
+++ bdd.sh
@@ -0,0 +1,288 @@
+#!/bin/sh
+
+### VARIABLES ####
+
+TRUE=0
+FALSE=1
+E_NOARGS=68
+VERBOSE=1
+DB_FILE="sh.db"
+DB_HEADER="neoBDSH V0.1\nLast update:   `date`"
+SEPARATOR=' #:@:# '
+KEY=""
+VALUE=""
+
+
+###########################
+####       FUNCTIONS   ####
+###########################
+
+usage ()
+{
+    echo "Usage:" >&2
+    cat<<EOF
+bdsh.sh [-v] [(-f | -c) <db_file>] (put (<key> | $<key>) (<value> | $<key>) |
+                                    del (<key> | $<key>) [<value> | $<key>] |
+                                    select [<expr> | $<key>]
+                                    flush)
+
+EOF
+}
+
+
+file_error ()
+{
+    echo "No base found : $DB_FILE" >&2
+    exit 1
+}
+
+set_output ()
+{
+    if [ ! -f "$DB_FILE" ]
+    then
+        echo "$DB_HEADER" > "$DB_FILE"
+    fi
+}
+
+
+db_put ()
+{
+    if [ -n "$1" ] && [ -n "$2" ]
+    then
+        KEY="$1"
+        VALUE="$2"
+        set_output
+        if [ `echo "$VALUE" | grep '^\$.*$'` ]
+        then
+            VALUE=`echo "$VALUE" | sed 's/^\$\([^ ]*\)/\1/g'`
+            SEARCH_VALUE=`grep "^$VALUE $SEPARATOR" "$DB_FILE"`
+            if [ -z "$SEARCH_VALUE" ]
+            then
+                echo "No such key : \$<$VALUE>" >&2
+                exit 1
+            else
+                VALUE=`echo "$SEARCH_VALUE" | sed 's/.* \([^ ]*\)$/\1/g'`
+            fi
+        fi
+        ### if in form $<key> ###
+        if [ `echo "$KEY" | grep '^\$.*$'` ]
+        then
+            ### parse value of $key ###
+            KEY=`echo "$KEY" | sed 's/^\$\([^ ]*\)/\1/g'`
+            SEARCH_KEY=`grep "^$KEY $SEPARATOR" "$DB_FILE"`
+            if [ -z "$SEARCH_KEY" ]
+            then        ### KEY NOT FOUND ###
+                echo "No such key : \$<$KEY>" >&2
+                exit 1
+            else
+                ### KEY FOUND ###
+                KEY=`echo "$SEARCH_KEY" | sed 's/.* \([^ ]*\)$/\1/g'`
+                SEARCH_KEY=`grep "^$KEY $SEPARATOR" "$DB_FILE"`
+                if [ -z "$SEARCH_KEY" ]
+                then
+                    echo "$KEY $SEPARATOR $VALUE" >> "$DB_FILE"
+                else
+                    cat "$DB_FILE" | sed "/^$KEY $SEPARATOR/ s/[^ ]*$/$VALUE/g" > "$DB_FILE.temp"
+                    cat $DB_FILE.temp > $DB_FILE && rm $DB_FILE.temp
+                fi
+            fi
+            exit 0
+        fi
+        ### KEY in form <KEY>
+        SEARCH_KEY=`grep "^$KEY $SEPARATOR" "$DB_FILE"`
+        if [ -n "$SEARCH_KEY" ]  ### if key exists
+        then                     ### Replacing key
+            cat "$DB_FILE" | sed "/^$KEY / s/[^ ]*$/$VALUE/g" "$DB_FILE.temp"
+        else                     ### Creating new key
+            echo "$KEY $SEPARATOR $VALUE" >> "$DB_FILE"
+        fi
+    fi
+}
+
+db_del ()
+{
+    KEY="$1"
+    VALUE="$2"
+    ### if in form $key
+    if [ `echo "$KEY" | grep '^\$.*$'` ]
+    then
+        ### parse value of $key ###
+        KEY=`echo "$KEY" | sed 's/^\$\([^ ]*\)/\1/g'`
+        SEARCH_KEY=`grep "^$KEY $SEPARATOR" "$DB_FILE"`
+        KEY=`echo "$SEARCH_KEY" | sed 's/.* \([^ ]*\)$/\1/g'`
+    fi
+    if [ -z "$2" ]
+    then
+        ### searching key
+        SEARCH_KEY=`grep "^$KEY $SEPARATOR" "$DB_FILE"`
+        ### KEY FOUND ###
+        if [ -n "$SEARCH_KEY" ]
+        then
+            VALUE=`echo "$SEARCH_KEY" | sed 's/.* \([^ ]*\)$/\1/g'`
+            cat "$DB_FILE" | sed "/^$KEY / s/[^ ]*$//g" > "$DB_FILE.temp"
+            cat $DB_FILE.temp > $DB_FILE && rm $DB_FILE.temp
+        fi
+    else
+        ### Detecting value form
+        if [ `echo "$VALUE" | grep '^\$.*$'` ]
+        then
+            VALUE=`echo "$VALUE" | sed 's/^\$\([^ ]*\)/\1/g'`
+            SEARCH_VALUE=`grep "^$VALUE $SEPARATOR" "$DB_FILE"`
+            VALUE=`echo "$SEARCH_VALUE" | sed 's/.* \([^ ]*\)$/\1/g'`
+        fi
+        if [ -n "$VALUE" ]
+        then
+            cat "$DB_FILE" | sed "/^$KEY/d" > "$DB_FILE.temp"
+            cat $DB_FILE.temp > $DB_FILE && rm $DB_FILE.temp
+        fi
+    fi
+    exit 0
+}
+
+db_select ()
+{
+    if [ ! "$1" ]
+    then
+        echo "$1"
+        cat "$DB_FILE" | sed "s/^.* $SEPARATOR //g"
+        exit 0
+    else
+        KEY="$1"
+        ### $key state
+        if [ `echo "$KEY" | grep '^\$.*$'` ]
+        then
+            ### parse $key value ###
+            KEY=`echo "$KEY" | sed 's/^\$\([^ ]*\)/\1/g'`
+            SEARCH_KEY=`grep "^$KEY $SEPARATOR" "$DB_FILE"`
+            KEY=`echo "$SEARCH_KEY" | sed 's/.* \([^ ]*\)$/\1/g'`
+            SEARCH_KEY=`grep "^$KEY $SEPARATOR" "$DB_FILE"`
+            if [ -z "$SEARCH_KEY" ]
+            then
+                echo "No such key : \$<$KEY>" >&2
+                exit 1
+            fi
+            # VALUE=`echo "$SEARCH_KEY" | sed 's/.* \([^ ]*\)$/\1/g'`
+            VALUE=`echo "$SEARCH_KEY" | awk -F "$SEPARATOR" '{print $2}'`
+            if [ $VERBOSE = "0" ]
+            then
+                echo "<$KEY>=<$VALUE>"
+            else
+                echo $VALUE
+            fi
+            exit 0
+        else
+            if [ $VERBOSE = "0" ]
+            then
+                TMP=`grep ".*$KEY.* $SEPARATOR" "$DB_FILE"`
+                if [ -z "$TMP" ]
+                then
+                    echo "$KEY="
+                else
+                    grep ".*$KEY.* $SEPARATOR" "$DB_FILE" | sed "s/ $SEPARATOR /=/g"
+                fi
+                exit 0
+            else
+                grep ".*$KEY.* $SEPARATOR" "$DB_FILE"  | sed "s/^.* $SEPARATOR //g"
+            fi
+            exit 0
+        fi
+    fi
+}
+
+dump ()
+{
+    [ -f sh.db ] && cat sh.db
+    [ ! -f sh.db ] && cat *.db
+}
+
+#############################
+#### PROGRAM STARTS HERE ####
+#############################
+
+if [ ! "$1" ]
+then
+    echo "Syntax error : put" >&2
+    exit $E_NOARGS
+fi
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+    -v)
+        VERBOSE=0
+        ;;
+    -c|-f)
+        DB_FILE="$2"
+        if [ -n "$2" ]
+        then
+            if [ ! -f "$DB_FILE" ] && [ "$1" = "-f" ]
+            then
+                echo "No base found : $DB_FILE " >&2
+                exit 1
+            else
+                shift
+            fi
+        else
+            usage
+            exit $E_NOARGS
+        fi
+        ;;
+    -*)
+        usage
+        exit $E_NOARGS
+        ;;
+    put)
+        if [ $# -ne 3 ]
+        then
+            echo "Syntax error : put" >&2
+            exit 1
+        else
+            ### Put cmd ###
+            db_put "$2" "$3"
+            shift;shift
+        fi
+        ;;
+    del)
+        if [ -n "$2" ] && [ ! $4 ]
+        then
+            ### Del cmd ###
+            db_del "$2" "$3"
+            shift;shift
+        else
+            echo "Syntax error : put" >&2
+            exit $E_NOARGS
+        fi
+        ;;
+    select)
+        if [ $# -lt 3 ]
+        then
+            ### Select cmd ###
+            db_select "$2"
+            shift
+            exit 0
+        else
+            echo "Syntax error : select" >&2
+            exit $E_NOARGS
+        fi
+        ;;
+    flush)
+        if [ "$#" -eq "1" ]
+        then
+            ### Flush cmd ###
+            echo "$DB_HEADER" > "$DB_FILE"
+            exit 0
+        else
+            usage
+            exit $E_NOARGS
+        fi
+        ;;
+    dump)
+        dump
+        ;;
+    *)
+        usage
+        break;;
+    esac
+    shift
+done
+
+#END
---
+++ test_bdd.py
@@ -0,0 +1,75 @@
+import unittest
+import itertools
+import subprocess
+import string
+PATH = "./bdd.sh"
+
+
+class BDDTestCase(unittest.TestCase):
+
+    def makeATest(self, params):
+        self.output = subprocess.Popen(' '.join([PATH] + params),
+                                       stdout=subprocess.PIPE, shell=True).communicate()[0]
+        return self.output
+
+    def putTest(self, key, value):
+        self.makeATest(['put', str(key), "'" + str(value) + "'"])
+
+    def delTest(self, key, value=None):
+        self.makeATest(['del', str(key), str(value)])
+
+    def selectTest(self, value):
+        return (self.makeATest(['select', str(value)]))
+
+    def flushTest(self):
+        self.makeATest(['flush'])
+
+    def testFlush(self):
+        self.flushTest()
+
+    def testPut(self):
+        self.putTest(0, "salut")
+
+    def testSelect(self):
+        self.selectTest('"*"')
+        self.assertEqual(self.output, 'salut\n')
+
+    def testDel(self):
+        self.delTest(1)
+        self.selectTest('"*"')
+        self.assertEqual(self.output, '')
+
+    def testx2(self):
+        self.flushTest()
+        dic = ['A', 'Z', 'E', 'R', 'T', 'Y']
+        for key, value in enumerate(dic):
+            self.putTest(key, value)
+        self.selectTest('"*"')
+        self.assertEqual(self.output, 'A\nZ\nE\nR\nT\nY\n')
+        self.delTest(1, 'Z')
+        self.selectTest('"*"')
+        self.assertEqual(self.output, 'A\nE\nR\nT\nY\n')
+        self.flushTest()
+        self.selectTest('"*"')
+        self.assertEqual(self.output, '')
+
+    def testy3(self):
+        self.flushTest()
+        word_list = []
+        for xs in itertools.product(string.ascii_lowercase, repeat=2):
+            word_list.append(''.join(xs))
+        for key, value in enumerate(word_list):
+            self.putTest(key, value)
+        self.flushTest()
+
+    def testz4(self):
+        self.flushTest()
+        self.putTest(0, "Hello World")
+        self.putTest(1, 0)
+        self.selectTest("'$1'")
+        self.assertEqual(self.output, 'Hello World\n')
+        self.flushTest()
+
+
+if __name__ == '__main__':
+    unittest.main()
